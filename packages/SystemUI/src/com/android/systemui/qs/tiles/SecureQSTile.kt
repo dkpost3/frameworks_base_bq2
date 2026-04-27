yaap/frameworks_base_old

base: Add a toggle for secure tiles on keyguard [1/2]

packages/SystemUI/src/com/android/systemui/qs/tiles/SecureQSTile.kt
@@ -17,6 +17,7 @@ package com.android.systemui.qs.tiles
 
 import android.os.Handler
 import android.os.Looper
+import android.provider.Settings
 import android.view.View
 import com.android.systemui.plugins.qs.QSTile
 import com.android.systemui.qs.QSHost
@@ -42,7 +43,9 @@ internal abstract class SecureQSTile<TState : QSTile.State> protected constructo
     protected abstract fun handleClick(view: View?, keyguardShowing: Boolean)
 
     override fun handleClick(view: View?) {
-        handleClick(view, mKeyguard.isMethodSecure && mKeyguard.isShowing)
+        val enabled: Boolean = Settings.Secure.getInt(mContext.getContentResolver(),
+            Settings.Secure.QSTILE_REQUIRES_UNLOCKING, 1) == 1
+        handleClick(view, mKeyguard.isMethodSecure && mKeyguard.isShowing && enabled)
     }
 
     protected fun checkKeyguard(view: View?, keyguardShowing: Boolean): Boolean {

dkpost3/frameworks_base_bq2

Change-Id: I4e53945d1cace76eb399d383b2addcddd27f2927
Signed-off-by: Jackeagle <jackeagle102@gmail.com>
Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>
Signed-off-by: Dmitrii <bankersenator@gmail.com>

packages/SystemUI/src/com/android/systemui/qs/tiles/SecureQSTile.kt
@@ -17,6 +17,7 @@ package com.android.systemui.qs.tiles
 
 import android.os.Handler
 import android.os.Looper
+import android.provider.Settings
 import com.android.systemui.animation.Expandable;
 import com.android.systemui.plugins.qs.QSTile
 import com.android.systemui.qs.QSHost
@@ -43,7 +44,9 @@ abstract class SecureQSTile<TState : QSTile.State> protected constructor(
     protected abstract fun handleClick(expandable: Expandable?, keyguardShowing: Boolean)
 
     override fun handleClick(expandable: Expandable?) {
-        handleClick(expandable, keyguardController.isMethodSecure && keyguardController.isShowing)
+        val enabled: Boolean = Settings.Secure.getInt(mContext.getContentResolver(),
+            Settings.Secure.QSTILE_REQUIRES_UNLOCKING, 1) == 1
+        handleClick(expandable, keyguardController.isMethodSecure && keyguardController.isShowing && enabled)
     }
 
     protected fun checkKeyguard(expandable: Expandable?, keyguardShowing: Boolean): Boolean {